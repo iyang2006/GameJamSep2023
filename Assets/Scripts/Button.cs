add0645f5d156a042a7c7e61623dcdad73e86b3d
iyang2006 <yang2006@purdue.edu>
2023-09-16 11:46:28 -0400

    sdf

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Button : MonoBehaviour
 {
+    GameObject platform;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,10 @@ public class Button : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //collision.gameObject.GetComponent<tag>
     }
 }
