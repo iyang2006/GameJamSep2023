bb9d7fa1f77f0eb1f8f6dddf9b077e547832591c
theNareynater <whitacre@purdue.edu>
2023-09-16 19:32:50 -0400

    Button & Platform

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -1,15 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Button : MonoBehaviour
 {
-    GameObject platform;
+    [SerializeField] private GameObject targetPlatform;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -18,8 +20,26 @@ public class Button : MonoBehaviour
         
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider collider)
     {
-        //collision.gameObject.GetComponent<tag>
+        Debug.Log("Collision Detected: " + collider.gameObject.layer + " | " + collider.gameObject.tag);
+        if (collider.gameObject.layer == 10)
+        {
+            Debug.Log("Button Entered");
+            Platform platform = targetPlatform.GetComponent<Platform>();
+            platform.Activate();
+        }
     }
+
+    private void OnTriggerExit(Collider collider)
+    {
+        if (collider.gameObject.layer == 10)
+        {
+            Debug.Log("Button Exited");
+            Platform platform = targetPlatform.GetComponent<Platform>();
+            platform.Deactivate();
+        }
+    }
+
+
 }
